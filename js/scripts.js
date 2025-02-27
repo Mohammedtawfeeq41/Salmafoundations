7f3a9bdd6212067ee6ab6bc041386732e8a78a3c
MOHAMMED TAWFEEQ <133195617+Mohammedtawfeeq41@users.noreply.github.com>
2025-02-27 16:00:53 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -355,3 +355,20 @@ document.addEventListener("DOMContentLoaded", function () {
     sessionStorage.setItem("welcomePopupShown", "true");
   }
 });
+
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  emailjs.sendForm("service_s6yri9i", "template_ujst02r", this, "0-lpnAN5_EjDzDavm")
+      .then(
+          function (response) {
+              console.log("SUCCESS!", response.status, response.text);
+              alert("Message sent successfully!");
+              document.getElementById("contact-form").reset();
+          },
+          function (error) {
+              console.log("FAILED...", error);
+              alert("Failed to send message. Please try again.");
+          }
+      );
+});
